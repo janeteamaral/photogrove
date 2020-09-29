23c4b5bd5ecfcf544d140b6b166ad4fd3d5e520a
janeteamaral <34576312+janeteamaral@users.noreply.github.com>
2020-09-29 12:01:45 -0300

    Add files via upload

---
+++ build/precache-manifest.cdcc4ca3e11ba5a535926e51a26a615a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15202198a73eae096196bdaa60d0e28d",
+    "url": "/photogrove/index.html"
+  },
+  {
+    "revision": "056786b8ad1d238dc541",
+    "url": "/photogrove/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "c44b046c6ecb6c6631a1",
+    "url": "/photogrove/static/css/main.4a75b481.chunk.css"
+  },
+  {
+    "revision": "056786b8ad1d238dc541",
+    "url": "/photogrove/static/js/2.5faf8c2f.chunk.js"
+  },
+  {
+    "revision": "3a9cdac41c7e5784566d9b451999051e",
+    "url": "/photogrove/static/js/2.5faf8c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44b046c6ecb6c6631a1",
+    "url": "/photogrove/static/js/main.fe063591.chunk.js"
+  },
+  {
+    "revision": "7d03a463fe0fd5751b4c",
+    "url": "/photogrove/static/js/runtime-main.d58b574c.js"
+  },
+  {
+    "revision": "fd02f3ede7d8608e6fcb603873ff8066",
+    "url": "/photogrove/static/media/LogoGrove2.fd02f3ed.png"
+  },
+  {
+    "revision": "65ffa5ba8be30c2385827338d50a227b",
+    "url": "/photogrove/static/media/sprite.65ffa5ba.png"
+  }
+]);
